caaac6eee17834a6c7c3575ab4e57a60ff784981
Federico G. Schwindt <fgsch@lodoss.net>
2014-06-10 15:17:06 +0100

    Reduce changes with master

--- src/vmod_geoip.c
+++ src/vmod_geoip.c
@@ -7,7 +7,6 @@
  * This code is licensed under a MIT-style License, see file LICENSE
 */
 
-
 #include <stdlib.h>
 #include <GeoIP.h>
 
@@ -17,20 +16,12 @@
 
 #include "vcc_if.h"
 
-
 // The default string in case the GeoIP lookup fails
 #define GI_UNKNOWN_STRING "Unknown"
 
-
-int
-init_function(struct vmod_priv *pp, const struct VCL_conf *conf)
-{
-	// first call to lookup functions initializes pp
-	return (0);
-}
-
 static void
-init_priv(struct vmod_priv *pp) {
+init_priv(struct vmod_priv *pp)
+{
 	// The README says:
 	// If GEOIP_MMAP_CACHE doesn't work on a 64bit machine, try adding
 	// the flag "MAP_32BIT" to the mmap call. MMAP is not avail for WIN32.
@@ -39,88 +30,97 @@ init_priv(struct vmod_priv *pp) {
 	GeoIP_set_charset((GeoIP *)pp->priv, GEOIP_CHARSET_UTF8);
 }
 
-
 const char *
-vmod_country_code(struct sess *sp, struct vmod_priv *pp, const char *ip)
+vmod_country_code(struct sess *sp, struct vmod_priv *pp,
+    const char *ip)
 {
-	const char* country = NULL;
+	const char *country = NULL;
 
-	if (!pp->priv) {
+	if (!pp->priv)
 		init_priv(pp);
-	}
 
-	if (ip) {
+	if (ip)
 		country = GeoIP_country_code_by_addr((GeoIP *)pp->priv, ip);
-	}
 
-	return(WS_Dup(sp->wrk->ws, (country ? country : GI_UNKNOWN_STRING)));
+	return (WS_Dup(sp->wrk->ws, (country ? country : GI_UNKNOWN_STRING)));
 }
 
 const char *
-vmod_client_country_code(struct sess *sp, struct vmod_priv *pp) {
-	return vmod_country_code(sp, pp, VRT_IP_string(sp, VRT_r_client_ip(sp)));
-}
-
-const char *
-vmod_ip_country_code(struct sess *sp, struct vmod_priv *pp, struct sockaddr_storage *ip) {
-	return vmod_country_code(sp, pp, VRT_IP_string(sp, ip));
-}
-
-
-const char *
-vmod_country_name(struct sess *sp, struct vmod_priv *pp, const char *ip)
+vmod_client_country_code(struct sess *sp, struct vmod_priv *pp)
 {
-	const char* country = NULL;
+	return (vmod_country_code(sp, pp, VRT_IP_string(sp,
+		VRT_r_client_ip(sp))));
+}
 
-	if (!pp->priv) {
+const char *
+vmod_ip_country_code(struct sess *sp, struct vmod_priv *pp,
+    struct sockaddr_storage *ip)
+{
+	return (vmod_country_code(sp, pp, VRT_IP_string(sp, ip)));
+}
+
+const char *
+vmod_country_name(struct sess *sp, struct vmod_priv *pp,
+    const char *ip)
+{
+	const char *country = NULL;
+
+	if (!pp->priv)
 		init_priv(pp);
-	}
 
-	if (ip) {
+	if (ip)
 		country = GeoIP_country_name_by_addr((GeoIP *)pp->priv, ip);
-	}
 
-	return(WS_Dup(sp->wrk->ws, (country ? country : GI_UNKNOWN_STRING)));
+	return (WS_Dup(sp->wrk->ws, (country ? country : GI_UNKNOWN_STRING)));
 }
 
 const char *
-vmod_client_country_name(struct sess *sp, struct vmod_priv *pp) {
-	return vmod_country_name(sp, pp, VRT_IP_string(sp, VRT_r_client_ip(sp)));
+vmod_client_country_name(struct sess *sp, struct vmod_priv *pp)
+{
+	return (vmod_country_name(sp, pp, VRT_IP_string(sp,
+		VRT_r_client_ip(sp))));
 }
 
 const char *
-vmod_ip_country_name(struct sess *sp, struct vmod_priv *pp, struct sockaddr_storage *ip) {
-	return vmod_country_name(sp, pp, VRT_IP_string(sp, ip));
+vmod_ip_country_name(struct sess *sp, struct vmod_priv *pp,
+    struct sockaddr_storage *ip)
+{
+	return (vmod_country_name(sp, pp, VRT_IP_string(sp, ip)));
 }
 
-
 const char *
-vmod_region_name(struct sess *sp, struct vmod_priv *pp, const char *ip)
+vmod_region_name(struct sess *sp, struct vmod_priv *pp,
+    const char *ip)
 {
 	GeoIPRegion *gir;
-	const char* region = NULL;
+	const char *region = NULL;
 
-	if (!pp->priv) {
+	if (!pp->priv)
 		init_priv(pp);
-	}
 
 	if (ip) {
-		if (gir = GeoIP_region_by_addr((GeoIP *)pp->priv, ip)) {
-			region = GeoIP_region_name_by_code(gir->country_code, gir->region);
-			// TODO: is gir* a local copy or the actual record?
+		if ((gir = GeoIP_region_by_addr((GeoIP *)pp->priv, ip))) {
+			region =
+			    GeoIP_region_name_by_code(gir->country_code,
+			    gir->region);
+			// TODO: is gir * a local copy or the actual record?
 			GeoIPRegion_delete(gir);
 		}
 	}
 
-	return(WS_Dup(sp->wrk->ws, (region ? region : GI_UNKNOWN_STRING)));
+	return (WS_Dup(sp->wrk->ws, (region ? region : GI_UNKNOWN_STRING)));
 }
 
 const char *
-vmod_client_region_name(struct sess *sp, struct vmod_priv *pp) {
-	return vmod_region_name(sp, pp, VRT_IP_string(sp, VRT_r_client_ip(sp)));
+vmod_client_region_name(struct sess *sp, struct vmod_priv *pp)
+{
+	return (vmod_region_name(sp, pp, VRT_IP_string(sp,
+	    VRT_r_client_ip(sp))));
 }
 
 const char *
-vmod_ip_region_name(struct sess *sp, struct vmod_priv *pp, struct sockaddr_storage *ip) {
-	return vmod_region_name(sp, pp, VRT_IP_string(sp, ip));
+vmod_ip_region_name(struct sess *sp, struct vmod_priv *pp,
+    struct sockaddr_storage *ip)
+{
+	return (vmod_region_name(sp, pp, VRT_IP_string(sp, ip)));
 }
